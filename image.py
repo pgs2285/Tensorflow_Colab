270c5a1c8b6cb78d6126b96b72e626e013e91211
pgs2285 <wldnjs2284@naver.com>
2021-02-22 15:23:44 +0900

    2021-02-22

--- image.py
+++ image.py
@@ -4,7 +4,9 @@ import PIL #python image library
 import PIL.Image
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from tensorflow.keras import layers
 import pathlib
+import matplotlib.pyplot as plt
 
 dataset_url =  "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file(origin=dataset_url,
@@ -17,4 +19,64 @@ image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
 roses = list(data_dir.glob('roses/*'))
-PIL.Image.SAVE(str(roses[0]))
+
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+#데이터 학습
+
+class_names = train_ds.class_names
+
+normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255) #RGB채널 값은 0~255 사이에 있으므로, 이상적이지 않음, 즉 표준화 필요
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds)) #반복자와 생성기룰 아용해 모두 꺼냄
+first_image = image_batch[0]
+######################################3
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+num_classes = 5
+
+model = tf.keras.Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(
+  train_ds,
+  batch_size=batch_size,
+  validation_data=val_ds,
+  epochs=3
+)
